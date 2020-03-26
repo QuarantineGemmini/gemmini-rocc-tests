37a11db0e386a7b65498ff333c5ec99fe1c5232f
Sam Steffl <sam@ssteffl.com>
2020-03-25 22:43:53 -0700

    pk, baremetal, linux workloads all supported. huge Makefile refactor

--- build.sh
+++ build.sh
@@ -14,11 +14,4 @@ if [ ! -d "build" ] ; then
     fi
 fi
 
-cd build
-
-if [[ $(which riscv64-unknown-linux-gnu-gcc) ]] ; then
-    make -j
-else
-    make -j BAREMETAL_ONLY=1
-fi
-
+cd build && make
