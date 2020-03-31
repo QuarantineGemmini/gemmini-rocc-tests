3c8e863cff526cb943d5d8d283553be3494a3194
Sam Steffl <sam@ssteffl.com>
2020-03-31 17:20:24 +0000

    firesim added to automation scripts

--- build.sh
+++ build.sh
@@ -3,15 +3,16 @@
 set -euo pipefail
 
 if [ ! -d "build" ] ; then
-    autoconf && \
-        mkdir build && cd build && \
-        ../configure &&
-        cd ..
+  autoconf
+  mkdir build
+  cd build
+  ../configure
+  cd ..
 
-    if [ $? -ne 0 ] ; then
-        echo $0 failed
-        exit 1
-    fi
+  if [ $? -ne 0 ] ; then
+    echo $0 failed
+    exit 1
+  fi
 fi
 
 cd build && make
